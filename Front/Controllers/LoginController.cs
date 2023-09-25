047b99ec9a53bb3accc852f20a2bee37614a8395
Paulo Silva <pauloecomercial367@gmail.com>
2023-09-25 19:45:46 -0300

    :sparkle: Recuperando  erros

--- Front/Controllers/LoginController.cs
+++ Front/Controllers/LoginController.cs
@@ -10,6 +10,14 @@ public class LoginController : Controller
     [HttpPost]
     public IActionResult Login(LoginViewModel viewModel)
     {
+        if (!ModelState.IsValid)
+        {
+            var errors = ModelState.Values.SelectMany(v => v.Errors)
+                                     .Select(e => e.ErrorMessage)
+                                     .ToList();
+            return BadRequest(errors);
+        }
+
         return Content(viewModel.Email + " | | " + viewModel.Password);
     }
 }
--- Front/ModelView/LoginViewModel.cs
+++ Front/ModelView/LoginViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace Front.ModelView;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Front.ModelView;
 
 public class LoginViewModel
 {
+    [Required(ErrorMessage = "É preciso informar o e-mail para continuar.")]
+    [EmailAddress(ErrorMessage = "É preciso informar um e-mail válido para continuar")]
     public string Email { get; set; }
+    [Required(ErrorMessage = "É preciso informar a senha para continuar")]
     public string Password { get; set; }
 }
