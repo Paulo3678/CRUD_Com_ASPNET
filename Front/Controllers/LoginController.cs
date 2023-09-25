a47ff6d3e827621d488046e053ae3bb06ceb54df
Paulo Silva <pauloecomercial367@gmail.com>
2023-09-25 19:37:17 -0300

    :sparkles: Carregando dados da view

---
+++ Front/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Front.ModelView;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Front.Controllers;
+
+public class LoginController : Controller
+{
+
+    public IActionResult Index() => View();
+    [HttpPost]
+    public IActionResult Login(LoginViewModel viewModel)
+    {
+        return Content(viewModel.Email + " | | " + viewModel.Password);
+    }
+}
